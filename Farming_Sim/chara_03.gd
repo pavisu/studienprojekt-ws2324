b7d8df12172d1c067f879735ee780056c85f574b
ralfze <razeit01@outlook.com>
2024-01-14 20:28:17 +0100

    Add some example plant

--- Farming_Sim/chara_03.gd
+++ Farming_Sim/chara_03.gd
@@ -8,6 +8,8 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const LERP_VAL = .15
+const JUMP_VAL= 5.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -32,15 +34,23 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	# check for jump
+	var space_pressed = Input.is_key_pressed(KEY_SPACE)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
 	if direction:
 		velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VAL)
 		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
+		# Jump if space_pressed and character is on the floor
+		if is_on_floor() and space_pressed:
+			velocity.y = JUMP_VAL
 		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
 		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
+		# Jump if space_pressed and character is on the floor
+		if is_on_floor() and space_pressed:
+			velocity.y = JUMP_VAL
 		
 	anim_tree.set("parameters/BlendSpace1D/blend_position",Vector2(velocity.x,velocity.z).length() / SPEED)
 
