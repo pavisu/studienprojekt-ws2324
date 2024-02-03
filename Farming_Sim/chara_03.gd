0f0fa112d32e42eaf8a752a939f17c9314d72089
ralfze <razeit01@outlook.com>
2024-02-03 23:29:24 +0100

    Change Raycast and Camera

--- Farming_Sim/chara_03.gd
+++ Farming_Sim/chara_03.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @onready var spring_arm_pivot = $SpringArmPivot
 @onready var spring_arm = $SpringArmPivot/SpringArm3D
 @onready var anim_tree =$AnimationTree
-@onready var ray = $RayCast3D
+@onready var ray = $SpringArmPivot/RayCast3D
 
 const SPEED = 5.0
 const LERP_VAL = .15
@@ -27,18 +27,26 @@ func _unhandled_input(event):
 		spring_arm_pivot.rotate_y(-event.relative.x * .005)
 		spring_arm.rotate_x(-event.relative.y * .005)
 		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
+		
+		# Ray movement with mouse
+		ray.rotate_x(-event.relative.y * .005)
+		ray.rotation.x = clamp(ray.rotation.x, PI/4, PI/2)
+		
+		# Character movement with camera
+		armature.rotate_y(-event.relative.x * .005)
+		
 
 func _physics_process(delta):
 	# Rotate to Player Character
 	var	coll_obj = ray.get_collider()
 	var	coll_point = ray.get_collision_point()
-	print(coll_obj)
-	print(coll_point)
+	#print(coll_obj)
+	#print(coll_point)
 	if coll_obj is GridMap:
 		#var cell_hit = coll_obj.local_to_map(to_local(coll_point))
 		#var item_id = coll_obj.get_cell_item(to_global(cell_hit))
 		var cell_hit = coll_point
-		print(cell_hit)
+		#print(cell_hit)
 		#if cell_hit.x <= 0:
 		#	cell_hit.x = floor(cell_hit.x)
 		#else:
@@ -58,9 +66,9 @@ func _physics_process(delta):
 			if coll_obj.get_cell_item(Vector3(cell_hit.x, cell_hit.y+1,cell_hit.z)) == -1:
 				coll_obj.set_cell_item(cell_hit.ceil(), 0, 0)
 		#print(to_global(cell_hit))
-		print(cell_hit)
-		print(item_id)
-		print("Gridmap hit")
+		#print(cell_hit)
+		#print(item_id)
+		#print("Gridmap hit")
 		#print(coll_obj.get_used_cells())
 		
 	# Add the gravity.
@@ -77,33 +85,34 @@ func _physics_process(delta):
 		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
 		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 		# Rotate Ray with character
+		#ray.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 		#var direction_ray = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		var direction_ray = (transform.basis * Vector3((2 * PI), 0, (2 * PI))).normalized()
-		direction_ray.x *= RAY_DISTANCE
-		direction_ray.z *= RAY_DISTANCE
+		#var direction_ray = (transform.basis * Vector3((2 * PI), 0, (2 * PI))).normalized()
+		#direction_ray.x *= RAY_DISTANCE
+		#direction_ray.z *= RAY_DISTANCE
 		#direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
-		direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y + (2.355))
+		#direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y + (2.355))
 		
-		ray.position.x = direction_ray.x
-		ray.position.z = direction_ray.z
+		#ray.position.x = direction_ray.x
+		#ray.position.z = direction_ray.z
 		#ray.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
 		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
 		# Rotate Ray with character
-		var camerapos = get_viewport().get_mouse_position()
-		print(spring_arm.rotation)
+		#var camerapos = get_viewport().get_mouse_position()
+		#print(spring_arm.rotation)
 		#print(camerapos)
 		#print(camerapos.position)
-		var direction_ray = (transform.basis * Vector3((2 * PI), 0, (2 * PI))).normalized()
-		direction_ray.x *= RAY_DISTANCE
-		direction_ray.z *= RAY_DISTANCE
+		#var direction_ray = (transform.basis * Vector3((2 * PI), 0, (2 * PI))).normalized()
+		#direction_ray.x *= RAY_DISTANCE
+		#direction_ray.z *= RAY_DISTANCE
 		#direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y + (PI/1.3333))
-		direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y + (2.355))
+		#direction_ray = direction_ray.rotated(Vector3.UP, spring_arm_pivot.rotation.y + (2.355))
 	
 			
-		ray.position.x = direction_ray.x
-		ray.position.z = direction_ray.z
+		#ray.position.x = direction_ray.x
+		#ray.position.z = direction_ray.z
 
 	if Input.is_action_pressed("jump"):
 			if is_on_floor():
